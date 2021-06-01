8cb1f04e794e77a4453a6e97e421d30ab8fb6c15
ecmnet <ecm@gmx.de>
2021-06-01 08:37:22 +0200

    Console fix

--- mavlquac/src/main/java/com/comino/mavlquac/console/Console.java
+++ mavlquac/src/main/java/com/comino/mavlquac/console/Console.java
@@ -112,27 +112,27 @@ public class Console implements Runnable {
 			return;
 		}
 
-		// otherwise execute it as OS level command
-		if(!control.getCurrentModel().sys.isStatus(Status.MSP_ARMED)) {
-			executeOSCommand(s);
-		}		
+//		// otherwise execute it as OS level command
+//		if(!control.getCurrentModel().sys.isStatus(Status.MSP_ARMED)) {
+//			executeOSCommand(s);
+//		}		
 	}
 
 
-	private void executeOSCommand(String command) {
-		try {
-			Process process;
-			process = Runtime.getRuntime().exec(command);
-			StreamGobbler streamGobbler = 
-					new StreamGobbler(process.getInputStream(), System.out::println);
-			Executors.newSingleThreadExecutor().submit(streamGobbler);
-			int exitCode = process.waitFor();
-			assert exitCode == 0;	
-		} catch (Exception e) {
-			MSPLogger.getInstance().writeLocalMsg("OS command '"+command+"' failed: "+e.getMessage(),
-					MAV_SEVERITY.MAV_SEVERITY_WARNING);
-		}
-	}
+//	private void executeOSCommand(String command) {
+//		try {
+//			Process process;
+//			process = Runtime.getRuntime().exec(command);
+//			StreamGobbler streamGobbler = 
+//					new StreamGobbler(process.getInputStream(), System.out::println);
+//			Executors.newSingleThreadExecutor().submit(streamGobbler);
+//			int exitCode = process.waitFor();
+//			assert exitCode == 0;	
+//		} catch (Exception e) {
+//			MSPLogger.getInstance().writeLocalMsg("OS command '"+command+"' failed: "+e.getMessage(),
+//					MAV_SEVERITY.MAV_SEVERITY_WARNING);
+//		}
+//	}
 
 	private static class StreamGobbler implements Runnable {
 		private InputStream inputStream;
